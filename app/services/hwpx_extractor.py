f96c59274065449826ca438e3ae0432fab8dda3e
DevRedz <94277988+DevRedz@users.noreply.github.com>
2025-06-11 14:03:20 +0900

    [Feature] 텍스트 추출 구현

--- app/services/hwpx_extractor.py
+++ app/services/hwpx_extractor.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 JAR_PATH = os.path.join(BASE_DIR, "python-hwpxlib-main", "hwpxlib-1.0.5.jar")
 LOADER_PATH = os.path.join(BASE_DIR, "python-hwpxlib-main", "hwpx_loader.py")
 
-def extract_text_from_hwpx(filename: str, file_bytes: bytes) -> str:
+def extract_text_from_hwpx(file_bytes: bytes) -> str:
     import tempfile
     import subprocess
 
@@ -24,4 +24,19 @@ def extract_text_from_hwpx(filename: str, file_bytes: bytes) -> str:
         return result.stdout
     finally:
         os.remove(tmp_path)
+
+
+# 테스트 코드
+# if __name__ == "__main__":
+#     filename = os.path.join(
+#         BASE_DIR,
+#         "python-hwpxlib-main",
+#         "2023년 디지털정부 발전유공 포상 추진계획.hwpx"
+#     )
+#     with open(filename, "rb") as f:
+#         file_bytes = f.read()
+#     text = extract_text_from_hwpx(
+#         file_bytes=file_bytes
+#     )
+#     print(text)
 
