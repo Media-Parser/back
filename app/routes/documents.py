27f46aff56ca4911a655309ff6f93e84fc3d403e
DevRedz <94277988+DevRedz@users.noreply.github.com>
2025-06-11 14:36:03 +0900

    Update documents.py

--- app/routes/documents.py
+++ app/routes/documents.py
@@ -4,15 +4,21 @@ from app.services.hwpx_extractor import extract_text_from_hwpx
 
 
 router = APIRouter()
+@router.get("/")
+def read_root():
+    return {"message": "Hello, Jinoh!"} # JSON 형식으로 반환
+    
 
 @router.post("/documents/upload")
-async def extract_hwpx_text(file: UploadFile = File(...)):
+async def documents_upload(file: UploadFile = File(...)):
     if not file.filename.endswith(".hwpx"):
         raise HTTPException(status_code=400, detail="Only .hwpx files are allowed.")
 
     contents = await file.read()
     try:
-        text = extract_text_from_hwpx(file.filename, contents)
+        text = extract_text_from_hwpx(contents)  # 파일명 인자 제거
         return {"text": text}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
